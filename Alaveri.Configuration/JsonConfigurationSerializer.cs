e0b8b49c01ac881e67a32461a56fcc73aaa97687
sshelby <sshelby@alaveri.org>
2022-09-22 16:45:02 -0700

    Configuration changes, added WPF support

---
+++ Alaveri.Configuration/JsonConfigurationSerializer.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alaveri.Configuration
+{
+    /// <summary>
+    /// Represents a configuration serializer that uses JSON serialization.
+    /// </summary>
+    /// <seealso cref="Alaveri.Configuration.ConfigurationSerializer" />
+    public class JsonConfigurationSerializer : ConfigurationSerializer
+    {
+        /// <summary>
+        /// Gets the file extension for this serialization format.
+        /// </summary>
+        /// <value>The file extension for this serialization format.</value>
+        public override string FileExtension { get => ".json"; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonConfigurationSerializer"/> class using the specified encoding.  Defaults to UTF8.
+        /// </summary>
+        /// <param name="encoding">The encoding.</param>
+        public JsonConfigurationSerializer(Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+            Encoding = encoding;
+        }
+
+        /// <summary>
+        /// Serializes a configuration to JSON.
+        /// </summary>
+        /// <typeparam name="TConfiguration">The type of the configuration to serialize.</typeparam>
+        /// <param name="config">The configuration to serialize.</param>
+        /// <returns>a byte array containing the serialized configuration data.</returns>
+        public override byte[] SerializeConfigurationData<TConfiguration>(TConfiguration config)
+        {
+            return Encoding.GetBytes(JsonConvert.SerializeObject(config, Formatting.Indented)); 
+        }
+
+        /// <summary>
+        /// Deserializes a configuration from JSON.
+        /// </summary>
+        /// <typeparam name="TConfiguration">The type of the configuration to deserialize.</typeparam>
+        /// <param name="configurationData">The configuration data to deserialize.</param>
+        /// <returns>a deserialized Configuration object of type <typeparamref name="TConfiguration" />.</returns>
+        public override TConfiguration DeserializeConfigurationData<TConfiguration>(byte[] configurationData)
+        {
+            return JsonConvert.DeserializeObject<TConfiguration>(Encoding.GetString(configurationData));
+        }
+
+    }
+}
